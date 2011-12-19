b4a3378581c7031d864b8b1c4ba10e6b77935393
Woody Gilk <woody.gilk@gmail.com>
2011-12-19 11:28:30 -0600

    Added additional handling for MailChimp meta data

--- classes/controller/demo/mailchimp.php
+++ classes/controller/demo/mailchimp.php
@@ -2,11 +2,32 @@
 
 class Controller_Demo_Mailchimp extends Controller_Demo_OAuth2 {
 
+	protected $api_endpoint;
+
+	public function before()
+	{
+		parent::before();
+
+		$this->api_endpoint = $this->session->get($this->key('api_endpoint'));
+	}
+
 	public function demo_metadata()
 	{
 		$meta = $this->provider->metadata($this->token);
 
+		$this->session->set($this->key('api_endpoint'), $meta->api_endpoint);
+
 		$this->content = Debug::vars($meta);
+	}
+
+	public function demo_logout()
+	{
+		if (Arr::get($_GET, 'confirm'))
+		{
+			$this->session->delete($this->key('api_endpoint'));
+		}
+
+		return parent::demo_logout();
 	}
 
 }
