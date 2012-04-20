84ad62a5fb692b05123e99d5701a16eb98a63640
Kelvin Luck <git@kelvinluck.com>
2012-04-20 10:48:14 +0100

    Updated config file so that it doesn't cause errors in Kohana 3.1

--- config/mailchimp.php
+++ config/mailchimp.php
@@ -1,11 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-/**
- * Mailchimp API Key
- */
-$config['api_key'] = 'YOUR MAILCHIMP API KEY';
-
-/**
- * Mailchimp List ID
- */
-$config['list_id'] = 'YOUR MAILCHIMP LIST ID';
+return array(
+	'api_key' => 'YOUR MAILCHIMP API KEY',
+	'list_id' => 'YOUR MAILCHIMP LIST ID',
+);
